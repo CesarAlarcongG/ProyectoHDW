63dc04631ff6093540c660ebaebbdc709d1b7154
Cesar <cesarjoelalarconguzman@gmail.com>
2025-06-02 17:57:36 -0500

    feat: funcion para obtener todos los recursos

--- src/main/java/com/negocio/ProyectoHerramientaDeDesarrolloWeb/service/RecursosService.java
+++ src/main/java/com/negocio/ProyectoHerramientaDeDesarrolloWeb/service/RecursosService.java
@@ -5,9 +5,11 @@ import com.negocio.ProyectoHerramientaDeDesarrolloWeb.persistence.repository.Rec
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RecursosService {
     @Autowired
     private RecursosRepository recursosRepository;
